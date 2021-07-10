ae401bb2abe997b08f089fd7424fd50b28d3e137
John A. Stevens <john@otherskylines.com>
2021-07-10 12:31:24 -0600

    Add slide in/out animation for API event list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -20,6 +21,7 @@ import { DayOfWeekPipe } from './pipes/day-of-week.pipe';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     TooltipModule
   ],
--- src/app/service-api-event-list/service-api-event-list.component.ts
+++ src/app/service-api-event-list/service-api-event-list.component.ts
@@ -1,11 +1,28 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MlbApiDataServiceEvent } from '../services/mlb-api-data-service-event';
 import { MlbApiDataService } from '../services/mlb-api-data.service';
+import {
+  trigger,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-service-api-event-list',
   templateUrl: './service-api-event-list.component.html',
-  styleUrls: ['./service-api-event-list.component.css']
+  styleUrls: ['./service-api-event-list.component.css'],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({transform: 'translateX(-100%)'}),
+        animate('200ms ease-in', style({transform: 'translateX(0%)'}))
+      ]),
+      transition(':leave', [
+        animate('200ms ease-in', style({transform: 'translateX(-100%)'}))
+      ])
+    ])
+  ]
 })
 export class ServiceApiEventListComponent implements OnInit {
 
